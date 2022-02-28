69460a2533ec7adb5601937e834b44d595e4ba27
Alcibíades Cabral Díaz <alcibiades@Alcibiadess-MacBook-Pro-2.local>
2022-02-28 19:55:33 +0000

    added alias store

--- svelte.config.js
+++ svelte.config.js
@@ -4,29 +4,30 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
-		vite: {
-			resolve: {
-				alias: {
-					$components: path.resolve('./src/components/'),
-					$atoms: path.resolve('./src/components/atoms/'),
-					$molecules: path.resolve('./src/components/molecules/'),
-					$organisms: path.resolve('./src/components/organisms/'),
-					$pages: path.resolve('./src/components/pages/')
-				}
-			}
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    },
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('./src/components/'),
+          $atoms: path.resolve('./src/components/atoms/'),
+          $molecules: path.resolve('./src/components/molecules/'),
+          $organisms: path.resolve('./src/components/organisms/'),
+          $pages: path.resolve('./src/components/pages/'),
+          $stores: path.resolve('./src/stores/')
+        }
+      }
+    }
+  }
 };
 
 export default config;
